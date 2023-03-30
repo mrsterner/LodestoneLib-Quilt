1acad737b04e21c9f656ac283d55563c4532dfaa
mrsterner <simon@sterner.dev>
2023-03-30 17:07:56 +0200

    1.19.4

--- src/main/java/com/sammy/lodestone/mixin/ItemRendererMixin.java
+++ src/main/java/com/sammy/lodestone/mixin/ItemRendererMixin.java
@@ -3,6 +3,7 @@ package com.sammy.lodestone.mixin;
 import com.sammy.lodestone.handlers.screenparticle.ScreenParticleHandler;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,12 +13,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ItemRenderer.class)
 final class ItemRendererMixin {
 
-	@Inject(method = "renderGuiItemModel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;scale(FFF)V", ordinal = 0))
-	private void lodestone$renderGuiItem(ItemStack pStack, int pX, int pY, BakedModel pBakedmodel, CallbackInfo ci) {
-		ScreenParticleHandler.renderItemStackEarly(pStack, pX, pY);
+	@Inject(method = "renderItem(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/item/ItemStack;IILnet/minecraft/client/render/model/BakedModel;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;scale(FFF)V", ordinal = 0))
+	private void lodestone$renderGuiItem(MatrixStack matrices, ItemStack stack, int x, int y, BakedModel model, CallbackInfo ci) {
+		ScreenParticleHandler.renderItemStackEarly(stack, x, y);
 	}
-	@Inject(method = "renderGuiItemModel", at = @At(value = "TAIL"))
-	private void lodestone$renderGuiItemLate(ItemStack pStack, int pX, int pY, BakedModel pBakedmodel, CallbackInfo ci) {
+	@Inject(method = "renderItem(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/item/ItemStack;IILnet/minecraft/client/render/model/BakedModel;)V", at = @At(value = "TAIL"))
+	private void lodestone$renderGuiItemLate(MatrixStack matrices, ItemStack stack, int x, int y, BakedModel model, CallbackInfo ci) {
 		ScreenParticleHandler.renderItemStackLate();
 	}
 }
